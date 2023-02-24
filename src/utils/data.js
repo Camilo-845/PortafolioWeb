74cb69e2a8cc1ed5d9ce10312d9b9006613f7694
Camilo S <camilo.sarmiento.amado@gmail.com>
2023-02-24 16:58:06 -0500

    skills data

--- src/utils/data.js
+++ src/utils/data.js
@@ -3,5 +3,73 @@ export const PROFILE_DATA = {
     "Derrollador Full Stack, dispuesto a afrontar retos y siempre aprender más.",
   about_me_2:
     "Comencé en el mundo de la programación en el 2020 con el programa del ministerio de las TIC de Colombia 'MisioinTic 2022', donde desarrollé habilidades en programación en Lenguajes como Python, Java, entre otros. Más recientemente me certifiqué como Full Stack Web Developer a través del BootCamp SoyHenry",
-  about_me_3: "Tengo un año de experiencia desarrollando proyectos, tanto por mi cuenta como en equipo. Siempre con una actitud colaborativa, dispuesto ayudar y recibir ayuda.",
+  about_me_3:
+    "Tengo un año de experiencia desarrollando proyectos, tanto por mi cuenta como en equipo. Siempre con una actitud colaborativa, dispuesto ayudar y recibir ayuda.",
+};
+
+export const SKILLS_DATA = {
+  principal: [
+    {
+      name: "JavaScript",
+      icon: "https://www.svgrepo.com/show/349419/javascript.svg",
+    },
+    {
+      name: "HTML",
+      icon: "https://www.svgrepo.com/show/452228/html-5.svg",
+    },
+    {
+      name: "CSS",
+      icon: "https://www.svgrepo.com/show/452185/css-3.svg",
+    },
+    {
+      name: "React",
+      icon: "https://www.svgrepo.com/show/452092/react.svg",
+    },
+    {
+      name: "PostgreSQL",
+      icon: "https://www.svgrepo.com/show/354200/postgresql.svg",
+    },
+    {
+      name: "Node Js",
+      icon: "https://www.svgrepo.com/show/452075/node-js.svg",
+    },
+    {
+      name: "MongoDB",
+      icon: "https://www.svgrepo.com/show/373845/mongo.svg",
+    },
+  ],
+  secondary: [
+    {
+      name: "Jira",
+      icon: "https://www.svgrepo.com/show/353935/jira.svg",
+    },
+    {
+      name: "Git",
+      icon: "https://www.svgrepo.com/show/452210/git.svg",
+    },
+    {
+      name: "GitHub",
+      icon: "https://www.svgrepo.com/show/475654/github-color.svg",
+    },
+    {
+      name: "Python",
+      icon: "https://www.svgrepo.com/show/452091/python.svg",
+    },
+    {
+      name: "SQL",
+      icon: "https://www.svgrepo.com/show/499816/database.svg",
+    },
+    {
+      name: "NoSQL",
+      icon: "https://www.svgrepo.com/show/134173/database.svg",
+    },
+    {
+      name: "Postman",
+      icon: "https://www.svgrepo.com/show/354202/postman-icon.svg",
+    },
+    {
+      name: "Figma",
+      icon: "https://www.svgrepo.com/show/452202/figma.svg",
+    },
+  ],
 };
