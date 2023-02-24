6d6c6889b8772aedc8ef598932086bbe4065962d
Camilo S <camilo.sarmiento.amado@gmail.com>
2023-02-24 17:59:41 -0500

    Skills component and CSS / responsive

--- src/Components/resume/skills/skills.jsx
+++ src/Components/resume/skills/skills.jsx
@@ -1,10 +1,34 @@
 import React from "react";
 import styles from "./Skills.module.css"
+import {SKILLS_DATA} from "../../../utils/data"
 
 function skills(){
     return (
         <div className={styles.mainContainer}>
-            <h1>Skills</h1>
+            <div className={styles.principalSkillsContainer}>
+                {(SKILLS_DATA.principal)?SKILLS_DATA.principal.map(el => {
+                    return (
+                        <div className={styles.principalSkill} key={el.name}>
+                            <img src={el.icon} alt={el.name}/>
+                            <h2>{el.name}</h2>
+                        </div>
+                    )
+                }):<></>}
+            </div>
+            <br />
+            <br />
+            <br />
+            <div className={styles.secondarySkillsContainer}>
+
+                {(SKILLS_DATA.secondary)?SKILLS_DATA.secondary.map(el => {
+                    return (
+                        <div className={styles.secondarySkill} key={el.name}>
+                            <img src={el.icon} alt={el.name}/>
+                            <h3>{el.name}</h3>
+                        </div>
+                    )
+                }):<></>}
+            </div>
         </div>
     )
 }
