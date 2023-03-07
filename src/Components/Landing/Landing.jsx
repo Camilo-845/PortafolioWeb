3bbf46107aa80872e0b4ce544b65796718d6cb04
Camilo S <camilo.sarmiento.amado@gmail.com>
2023-03-07 17:11:34 -0500

    landing cv links

--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -1,20 +1,28 @@
 import React from "react";
 import styles from "./Landing.module.css"
 import downloadImage from "../../assets/download-svgrepo-com.png"
+import CV from "../../assets/CV  EN.pdf"
+import toast, { Toaster } from 'react-hot-toast';
+
 
 function Landing(){
+    const notify = () => toast('Download');
     return (
         <div id="resume" className={styles.mainContainer}>
             <div className={styles.dataContainer}>
-                <h1>Hello!</h1><br /><br />
+                <h1>Hi!</h1><br /><br />
                 <h1>My name is Camilo <br/> Sarmiento</h1><br /><br />
                 <h1>I'm a Full Stack Developer</h1><br /><br />
                 <div className={styles.buttonsContainer}>
-                    <button>View CV</button>
-                    <button>
-                        <img className={styles.downloadImage} src={downloadImage} alt="" />
-                        CV
-                    </button>
+                    <a href={CV} target="_blank">
+                        <button>View CV</button>
+                    </a>
+                    <a href={CV} download="CV_Camilo_Sarmiento_FullStack" onClick={notify}>
+                        <button>
+                            <img className={styles.downloadImage} src={downloadImage} alt="" />
+                            CV
+                        </button>
+                    </a>
                     <a href="#contact"><button>Contact</button></a>
                 </div>
             </div>
--- src/utils/notifications.js
+++
@@ -1,7 +0,0 @@
-import toast, { Toaster } from 'react-hot-toast';
-
-export const notification =function(){
-}
-notification.prototype.success = function(message) {
-    toast.success(message)
-}
