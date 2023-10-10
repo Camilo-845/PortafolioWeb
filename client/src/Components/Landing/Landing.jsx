e867a6d52a4b4e248542b2d43635bbd6818c0809
Camilo S <camilo.sarmiento.amado@gmail.com>
2023-10-10 12:44:13 -0500

    Se añadio el CV en la verison de ingles y ademas de ruteo en el landing Page

--- client/src/Components/Landing/Landing.jsx
+++ client/src/Components/Landing/Landing.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./Landing.module.css"
 import downloadImage from "../../assets/download-svgrepo-com.png"
 import CV from "../../assets/CV - Camilo Sarmiento.pdf"
+import CV_EN from "../../assets/CV - CAMILO SARMIENTO_EN.pdf"
 import toast, { Toaster } from 'react-hot-toast';
 import { useSelector } from "react-redux";
 
@@ -16,10 +17,10 @@ function Landing(){
                 <h1>{(idiom=="EN")?"My name is Camilo":"Mi nombre es Camilo"}<br/> Sarmiento</h1><br /><br />
                 <h1>{(idiom=="EN")?"I'm a Full Stack Developer":"Soy desarrollador FullStack"}</h1><br /><br />
                 <div className={styles.buttonsContainer}>
-                    <a href={CV} target="_blank">
+                    <a href={(idiom=="EN")?CV_EN : CV} target="_blank">
                         <button>{(idiom=="EN")?"View":"Ver"} CV</button>
                     </a>
-                    <a href={(idiom=="EN")?CV:CV} download="CV_Camilo_Sarmiento_FullStack" onClick={notify}>
+                    <a href={(idiom=="EN")?CV_EN : CV} download="CV_Camilo_Sarmiento_FullStack" onClick={notify}>
                         <button>
                             <img className={styles.downloadImage} src={downloadImage} alt="" />
                             CV
